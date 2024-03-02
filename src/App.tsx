3bdc0428257f5b13d1596d61b166ddf84065b797
MARVIN <o.silverygold@gmail.com>
2024-03-02 15:05:59 +0300

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
+import Books from './components/Books';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import type { Book, BooksProps } from './lib/types';
 
 function App() {
-
+  const books: Book[] = [
+    { author: 'Eunniah Mbabazi', title: 'Breaking Down', year: 1968, category: 'Fiction' },
+    { author: 'Chinua Achebe', title: 'Things Fall Apart', year: 1975, category: 'Non-fiction' },
+    { author: 'Chinua Achebe', title: 'The River Between', year: 1960, category: 'Folklore' },
+    { author: 'Mbongeni Ngema', title: 'Themes of Sarafina', year: 1982, category: 'Science Fiction' },
+    { author: 'Marvin Nyalik', title: 'Lyrical Labyrinth', year: 2005, category: 'Fairytales' },
+  ];
+  
+  const booksData: BooksProps = { books: books }
+  
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello Word</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Books books={booksData}/>}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
