61593c58979bb00f5edb29066994b2db291e6c82
MARVIN <o.silverygold@gmail.com>
2024-03-03 18:54:27 +0300

    Define Rocket type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,12 @@ export type Book = {
 export type BooksProps = {
   books: Book[]
 }
+
+export type Rocket = {
+  id: string,
+  name: string,
+  country: string,
+  description: string,
+  company: string,
+  flickr_images: string[];
+}
